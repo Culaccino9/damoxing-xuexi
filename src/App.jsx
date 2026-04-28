9479c9f2a250ae5cc9f42fb90cea1311ed424659
WenjieHuang <83898020+Culaccino9@users.noreply.github.com>
2026-04-28 23:09:57 +0800

    feat: add chapter3 route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Chapter1 from './pages/chapter1.jsx'
 import Chapter2 from './pages/chapter2.jsx'
+import Chapter3 from './pages/chapter3.jsx'
 import KnowledgeGraph from './components/KnowledgeGraph.jsx'
 
 function Home(){
@@ -30,6 +31,7 @@ function Home(){
       <ul>
         <li><Link to="/chapter1">Chapter1：微调与部署</Link></li>
         <li><Link to="/chapter2">Chapter2：Prompt 与推理</Link></li>
+        <li><Link to="/chapter3">Chapter3：知识编辑</Link></li>
       </ul>
     </div>
   )
@@ -42,12 +44,14 @@ export default function App(){
       <nav>
         <Link to="/">首页</Link> | 
         <Link to="/chapter1">Chapter1 微调</Link> | 
-        <Link to="/chapter2">Chapter2 Prompt</Link>
+        <Link to="/chapter2">Chapter2 Prompt</Link> | 
+        <Link to="/chapter3">Chapter3 编辑</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/chapter1" element={<Chapter1/>} />
         <Route path="/chapter2" element={<Chapter2/>} />
+        <Route path="/chapter3" element={<Chapter3/>} />
       </Routes>
     </div>
   )
